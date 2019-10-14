58ae95a212f72c3477c9d67c62a48fe223de553f
RyotaNakajimakun <hope.for.world.peace0208@gmail.com>
2019-10-14 15:53:03 +0900

    [clean] trace

--- Golang/web_application/trace/trace.go
+++ Golang/web_application/trace/trace.go
@@ -13,7 +13,7 @@ type tracer struct {
 	out io.Writer
 }
 
-func (t *tracer) Trace (a ...interface{}) {
+func (t *tracer) Trace(a ...interface{}) {
 	t.out.Write([]byte(fmt.Sprint(a...)))
 	t.out.Write([]byte("\n"))
 }
@@ -22,9 +22,9 @@ func New(w io.Writer) Tracer {
 	return &tracer{out: w}
 }
 
-type nilTracer struct {}
+type nilTracer struct{}
 
-func (*nilTracer) Trace(a ...interface{}){}
+func (*nilTracer) Trace(a ...interface{}) {}
 
 // OffはTraceメソッドの呼び出しを無視するTraceを返します
 func Off() Tracer {
