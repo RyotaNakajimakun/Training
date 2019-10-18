e004b06fb2894650719152d8d2c49ac5be074001
RyotaNakajimakun <hope.for.world.peace0208@gmail.com>
2019-10-19 01:08:07 +0900

    [add] uiを変更
    ボタンを押したら関数を実行するまで実装

--- Javascript/React/single_page_applecation/src/App.jsx
+++ Javascript/React/single_page_applecation/src/App.jsx
@@ -2,60 +2,59 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
-import ButtonAppBar from './commponents/app_bar'
-import ProTip from './ProTip';
-import SignIn from "./signin";
-import TopPageBar from "./commponents/app_bar";
+import OutPutModule from "./commponents/output"
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import CachedIcon from '@material-ui/icons/Cached';
 
-function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Hello
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
+function CommandUpload() {
+    console.log(document.getElementById("input_command").value);
 }
 
 function InputCommandField() {
+    const f = () => CommandUpload();
     return (
-        <TextField
-            id="outlined-full-width"
-            label="Input Command"
-            style={{
-                margin: 8,
-                marginTop: 200
-            }}
-            placeholder="sudo systemd service restart network"
-            helperText=""
-            fullWidth
-            margin="normal"
-            variant="outlined"
-            InputLabelProps={{
-                shrink: true,
-            }}
-        />
-    )
+        <Grid container spacing={4}>
+            <Grid item xs={2}/>
+            <Grid item xs={6}>
+                <TextField
+                    id="input_command"
+                    label="Input Command"
+                    placeholder="モジュール化したいコマンドを入力してください"
+                    fullWidth
+                    margin="normal"
+                    variant="outlined"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+            </Grid>
+            <Grid item xs={2}>
+                <Button onClick={f} variant="contained" fullWidth={true} color="primary" type="submit" className=""
+                        style={{
+                            marginTop: 17,
+                            marginRight: "auto",
+                            marginLeft: "auto",
+                        }}>
+                    <CachedIcon style={{fontSize: 40}}/>
+                    変換
+                </Button>
+            </Grid>
+            <Grid item xs={2}/>
+        </Grid>
+    );
 }
 
 export default function App() {
     return (
-        <Container maxWidth="sm">
-            {/*<SignIn/>*/}
-            <ButtonAppBar/>
-            <InputCommandField/>
-            {/*<Box my={4}>*/}
-            {/*    <Typography variant="h4" component="h1" gutterBottom>*/}
-            {/*        Create React App v4-beta example*/}
-            {/*    </Typography>*/}
-            {/*    <ProTip/>*/}
-            {/*    <Copyright/>*/}
-            {/*</Box>*/}
+        <Container>
+            <Container>
+                <InputCommandField/>
+            </Container>
+            <Container maxWidth="sm">
+                <OutPutModule/>
+            </Container>
         </Container>
     );
 }
--- Javascript/React/single_page_applecation/src/index.jsx
+++ Javascript/React/single_page_applecation/src/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/styles';
+import {ThemeProvider} from '@material-ui/styles';
 import App from './App';
+import ButtonAppBar from './commponents/app_bar'
 import theme from './theme';
 
 ReactDOM.render(
     <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <App />
+        <CssBaseline/>
+        <ButtonAppBar/>
+        <App/>
     </ThemeProvider>,
     document.querySelector('#root'),
 );
