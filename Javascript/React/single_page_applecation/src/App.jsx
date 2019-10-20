125204cf2a0bbeb60984c08d780c34198c712dc1
RyotaNakajimakun <hope.for.world.peace0208@gmail.com>
2019-10-21 00:07:30 +0900

    [add] 変換ボタンのインターフェースを作成
    押下で出力を変更するまで実装

--- Javascript/React/single_page_applecation/src/App.jsx
+++ Javascript/React/single_page_applecation/src/App.jsx
@@ -1,60 +1,37 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField'
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import OutPutModule from "./commponents/output"
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import CachedIcon from '@material-ui/icons/Cached';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import InputCommandField from './commponents/default/input'
+import RenderModules from "./commponents/output"
+import Header from "./commponents/default/header";
 
-function CommandUpload() {
-    console.log(document.getElementById("input_command").value);
-}
 
-function InputCommandField() {
-    const f = () => CommandUpload();
-    return (
-        <Grid container spacing={4}>
-            <Grid item xs={2}/>
-            <Grid item xs={6}>
-                <TextField
-                    id="input_command"
-                    label="Input Command"
-                    placeholder="モジュール化したいコマンドを入力してください"
-                    fullWidth
-                    margin="normal"
-                    variant="outlined"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                />
-            </Grid>
-            <Grid item xs={2}>
-                <Button onClick={f} variant="contained" fullWidth={true} color="primary" type="submit" className=""
-                        style={{
-                            marginTop: 17,
-                            marginRight: "auto",
-                            marginLeft: "auto",
-                        }}>
-                    <CachedIcon style={{fontSize: 40}}/>
-                    変換
-                </Button>
-            </Grid>
-            <Grid item xs={2}/>
-        </Grid>
-    );
-}
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            values: ["TestValue", ""]
+        };
+    }
 
-export default function App() {
-    return (
-        <Container>
-            <Container>
-                <InputCommandField/>
-            </Container>
-            <Container maxWidth="sm">
-                <OutPutModule/>
-            </Container>
-        </Container>
-    );
+    CommandUpload() {
+        console.log(document.getElementById("input_command").value);
+
+        this.setState({
+            values: [document.getElementById("input_command").value,]
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <CssBaseline/>
+                <Header/>
+                <Container>
+                    <InputCommandField onClick={() => this.CommandUpload()}/>
+                    <RenderModules values={this.state.values}/>
+                </Container>
+            </div>
+        );
+    }
 }
--- Javascript/React/single_page_applecation/src/index.jsx
+++ Javascript/React/single_page_applecation/src/index.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import {ThemeProvider} from '@material-ui/styles';
 import App from './App';
-import ButtonAppBar from './commponents/app_bar'
-import theme from './theme';
+import Header from "./commponents/default/header";
+import Container from "@material-ui/core/Container";
 
-ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline/>
-        <ButtonAppBar/>
-        <App/>
-    </ThemeProvider>,
-    document.querySelector('#root'),
-);
+ReactDOM.render(<App/>, document.querySelector('#root'));
